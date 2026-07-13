82e8d4cc8881e4b5935bc75a75e1ee06e6d53d34
Anastasia <a.a.onuchina@mail.com>
2026-07-14 00:17:51 +0300

    Создала оконное приложение с вводом одной валюты Сделала выпадающий список популярных валют добавила словарь валют с полным наименованием

--- 02/02/Задания/Задание_5.py
+++ 02/02/Задания/Задание_5.py
@@ -4,6 +4,11 @@ from tkinter import messagebox as mb
 import requests
 import json
 
+def update_currency_label(event):
+    code = combobox.get()
+    name = currencies[code]
+    currency_label.config(text=name)
+
 def exchange():
     code = combobox.get()
     if code:
@@ -13,6 +18,7 @@ def exchange():
             data = response.json()
             if code in data['rates']:
                 exchange_rate = data['rates'][code]
+                currency_name = currencies[code]
                 mb.showinfo("Курс обмена", f"Курс к доллару: {exchange_rate:.1f} {code} за 1 доллар")
             else:
                 mb.showerror("Ошибка", f"Валюта {code} не найдена")
@@ -21,14 +27,29 @@ def exchange():
     else:
         mb.showwarning("Внимание", "Введите код валюты")
 
+currencies = {
+    "EUR": "Евро",
+    "JPY": "Японская йена",
+    "GBP": "Британский фунт стерлингов",
+    "AUD": "Австралийский доллар",
+    "CAD": "Канадский доллар",
+    "CHF": "Швейцарский франк",
+    "CNY": "Китайский юань",
+    "RUB": "Российский рубль",
+    "KZT": "Казахстанский тенге",
+    "UZS": "Узбекский сум"
+ }
+
 window = Tk()
 window.title("Курс обмена валюты к доллару")
 window.geometry("360x180")
 Label(text="Выберите код валюты:").pack(padx=10, pady=10)
-popular_currencies = ["EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "RUB", "KZT", "UZS"]
-combobox = ttk.Combobox(values=popular_currencies)
-combobox.pack(padx=10, pady=10)
 
+combobox = ttk.Combobox(values=list(currencies.keys()))
+combobox.pack(padx=10, pady=10)
+combobox.bind("<<ComboboxSelected>>", update_currency_label)
+currency_label = ttk.Label()
+currency_label.pack(padx=10, pady=10)
 Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10, pady=10)
 
 window.mainloop()
